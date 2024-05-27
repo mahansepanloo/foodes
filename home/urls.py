5efd55e0a691177f4c7de90eff8ff417f371a4d4
mahan <sepanloooo@gmail.com>
2024-05-27 15:07:38 +0330

    fix temp

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+
+app_name = 'home'
+urlpatterns = [
+    path('',views.Home.as_view(),name='home'),
+
+]
